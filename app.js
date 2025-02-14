6caf8a331530274edd4f48b70d0205aac5712769
jerryjerry2 <chmapikeat@gmail.com>
2025-02-14 08:56:29 +0700

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('successfully')
+    res.send('Hello my cron job')
 });
 
 app.listen(3001, () => {
