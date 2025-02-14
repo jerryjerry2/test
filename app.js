460a203d3c60e7aaff73a2e7058d649bc7fef7f2
jerryjerry2 <chmapikeat@gmail.com>
2025-02-14 08:19:29 +0700

    add file

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello nodejs')
+});
+
+app.listen(3001, () => {
+    console.log('Server Running on port 3001');
+})
